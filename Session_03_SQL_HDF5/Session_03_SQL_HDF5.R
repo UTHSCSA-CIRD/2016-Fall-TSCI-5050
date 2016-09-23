e9c701c856ee171d227a6b83fce845817e115796
Alex F. Bokov <bokov@uthscsa.edu>
2016-09-23 17:26:16 -0500

    Session 3

--- Session_03_SQL_HDF5/Session_03_SQL_HDF5.R
+++ Session_03_SQL_HDF5/Session_03_SQL_HDF5.R
@@ -3,4 +3,14 @@
 #' author: "Alex F. Bokov"
 #' date: "09/23/2016"
 #' ---
-#+ echo=FALSE,cache=TRUE
+#+ echo=TRUE,cache=TRUE
+#' Load libraries
+require(RSQLite);require(xtable);
+#' Go to the directory where the .db file lives on YOUR system
+setwd('/tmp/2016-Fall-TSCI-5050/');
+#' Open connection
+con <- dbConnect(SQLite(),'/tmp/2016-Fall-TSCI-5050/Session_03_SQL_HDF5/exampleinput.db');
+#' Select a table
+#+ results="asis"
+dat <- dbGetQuery(con,'select * from patient_dimension');
+print(xtable(dat),type='html');
