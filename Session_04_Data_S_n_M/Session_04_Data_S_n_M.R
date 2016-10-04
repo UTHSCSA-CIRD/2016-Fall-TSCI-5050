c373bd66d455b244c98b5fce6cae348973f314f5
Alex F. Bokov <bokov@uthscsa.edu>
2016-10-04 09:42:27 -0500

    Added starting files for session 04.

---
+++ Session_04_Data_S_n_M/Session_04_Data_S_n_M.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Data Storage and Manipulation"
+#' author: "Alex F. Bokov"
+#' date: "10/04/2016"
+#' ---
+#+ echo=TRUE,cache=TRUE
+#' Load libraries
+require(RSQLite);require(data.table);require(plyr);
+
