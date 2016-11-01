2030f4d50a7c84dee45841b32f0fd27aab552ca4
Nourhan Abdelfattah <Nourhan Abdelfattah>
2016-11-01 11:55:41 -0500

    new screen csv

--- Session_04_Data_S_n_M/Session_04_Data_S_n_M.R
+++ Session_04_Data_S_n_M/Session_04_Data_S_n_M.R
@@ -5,12 +5,63 @@
 #' ---
 #+ echo=TRUE,cache=TRUE
 #' Load libraries
-require(RSQLite);require(data.table);require(plyr);
+require(RSQLite);require(magrittr);require(data.table);require(plyr);require(ggplot2);
 
 #' Open connection (remember to change the path to match your location)
 con <- dbConnect(SQLite(),'/tmp/2016-Fall-TSCI-5050/Session_04_Data_S_n_M/Session_04_Nour.db');
 #' See tables
 print(.tmp<-dbListTables(con));
-#' Let's look at what is contained in `r .tmp[1]` and `r .tmp[2]`.
+#' Let's look at what is contained in `io` and `kc`.
 head(dbReadTable(con,'io'));
 head(dbReadTable(con,'kc'));
+
+dt <- dbReadTable(con,'kc');
+#' Convert to numeric because for some reason SQLite thinks its not
+dt[,] <- sapply(dt,as.numeric,simplify = F);
+#' Any missing values?
+is.na(dt);
+#' Apparently `is.numeric()` converts blanks to missing values (`NA`).
+#' Let's check:
+as.numeric(c('1','2.3','4','','13.8'));
+#' Fix it! It happens to be a zero in real life.
+dt[11,1] <- 0;
+#' Now reshape it to the long format
+reshape(dt,idvar='y',v.names='x',varying=list(2:9),direction='long') %>% 
+  setNames(c('Dose','Replicate','Signal')) -> dt1;
+#' ...notice the use of the `%>%` pipe operator (from the `magrittr` package)
+head(dt1);
+#' Check the data types
+sapply(dt1,class);
+#' Make replicate into a factor to prevent `lm()` and friends from over-interpreting it
+dt1$Replicate<- as.factor(dt1$Replicate);
+#' Now let's try some plots.
+gp <- ggplot(data=dt1,aes(x=Dose,y=Signal));
+gp + geom_line(aes(group=Replicate,colour=Replicate));
+gp + geom_line(aes(x=log(Dose),group=Replicate,colour=Replicate));
+gp + geom_line(aes(x=log(Dose),y=c(0,diff(Signal)),group=Replicate,colour=Replicate));
+#' But we need to diff _within_ each replicate. For that we can use `data.table`
+tab1 <- data.table(dt1);
+#' Do a bunch of interesting transformations.
+tab1[,cbind(.SD,                          # In data.table, .SD is the entire set 
+                                          # of rows currently being processed.
+                                          # It itself is also a `data.table`.
+            diffSignal=c(0,diff(Signal)), # Create a new column named 
+                                          # `diffSignal`,the first-order 
+                                          # difference for the Signal (the 
+                                          # inverse of cumsum).
+            logDose=log(Dose)),           # Create a new column named `logDose`,
+                                          # the log of the dose.
+                                          # This concludes the second argument
+                                          # of this data table subscripting 
+                                          # expression.
+     by=Replicate];    # The final argument, `by`, tells `data.table` to do the
+                       # transformations requested by the second argument 
+                       # separately for each `Replicate` 
+                       # (this matters for `diff()`)
+
+#' One last thing: dataset io
+dbReadTable(con,'io') %>% 
+  transform(x=as.numeric(x),y=as.numeric(y),z=as.numeric(z)) -> io;
+io$y_x <- with(io,y - x);
+plot(y_x~z,io,pch='.',cex=2,col='#00000060');
+
---
+++ Session_05_Plates/Session_05_platereading.R
@@ -0,0 +1,109 @@
+#' ---
+#' title: "96 Well Plates in R"
+#' author: "Alex F. Bokov"
+#' date: "October 18, 2016"
+#' ---
+
+#' Load some libraries we'll be using for this
+require('platetools');  
+require('xlsx');         # also reads xlsm and xls
+require('magrittr');     # provides the %>% operator
+require('nlme');         # mixed-effect models
+require('ggplot2');
+
+#' Declare session variables
+pmfile <- 'MI00200 MISSION microRNA Mimic v.17 Plate Map.xls';
+#' Unless you run Linux, always check out the git repo to the /tmp
+#' directory where you push and delete it after working, you will 
+#' likely want to modify these paths if nothing else
+path <- '/tmp/2016-Fall-TSCI-5050/'
+datapath <- paste0(path,'Session_05_Plates/screen/');
+setwd(path);
+
+#' Create  the well labels
+wells <- outer(LETTERS[1:8],t(1:12),paste0)[,1,]
+
+#' Read the plate map file
+read.xlsx(paste0(datapath,pmfile),sheetIndex = 1) %>% 
+  subset(Symbol!='EMPTY') %>% 
+  transform(ID=gsub('[^0-9]','',Plate.ID)) -> map;
+levels(map$Plate.Location) <- gsub('0([0-9])$','\\1',levels(map$Plate.Location))
+
+#' Function for converting from 8x12 layout to the 96x2 (or 384x2) one
+unplate <- function(dat,plate=96) {
+  nrow<-8; ncol<-12;
+  if(plate==384) {nrow<-2*nrow; ncol<-2*ncol;}
+  data.frame(well=num_to_well(1:plate),value=c(t(dat[1:nrow,1:ncol])))
+}
+
+#' We read the platest into a nested list-- top level are
+#' files, second level are replicates of the same plate,
+#' first 3 are controls, second three are vincristin
+(list.files(pattern = 'series.xlsm',path = datapath,full.names=T)->filenames) %>% 
+  # Outer pipeline run on the who set of plates
+  lapply(
+    function(xx) {
+      # Inner pipeline run on each plate
+      1:6 %>% 
+        lapply(function(yy) read.xlsx(xx,yy)) %>%
+        setNames(rep(paste0(c('c','c','c','v','v','v'),1:6)));
+      # End inner pipeline 
+    }) %>% 
+  setNames(paste0('pl',gsub('.*//([0-9]{1,2}) series.xlsm$','\\1',filenames))) -> dat;
+
+do.call('c',dat) %>% sapply(unplate,simplify=F) -> dat2;
+
+#' Combine everything into one big dataframe preserving
+#' replicate, plate, and treatment info
+sapply(names(dat2),function(xx) {
+  dat2[[xx]]$repid<-xx;dat2[[xx]]
+  },simplify=F) %>% do.call('rbind',.) -> dat3;
+
+dat3$ID <- gsub('^pl([0-9]{1,2}).*','\\1',dat3$repid);
+dat3$treat <- gsub('[^vc]','',dat3$repid);
+dat3$Plate.Location <- dat3$well;
+levels(dat3$Plate.Location) <- gsub('^([A-H])0([0-9])$','\\1\\2',levels(dat3$well));
+dat3$col<-gsub('[^0-9]','',dat3$Plate.Location);
+dat3$row<-gsub('[0-9]','',dat3$Plate.Location);
+dat3$nwell <- rep(1:96,length(unique(dat3$repid)));
+
+merge(dat3,transform(map[,c(1:9,13)],Plate.ID=as.character(Plate.ID)),
+      by = c('Plate.Location','ID'),all.x=T) %>% 
+  transform(Symbol=as.character(Symbol)) -> dat4;
+
+dat4[dat4$well %in% c('A12','B12'),'Symbol'] <- 'siPLK';
+
+dat4[grepl('^[A-H]1$',dat4$Plate.Location),'Symbol'] <- 'Mock';
+
+dat4[is.na(dat4$Symbol),'Symbol']<- 'Control';
+
+split(dat4,dat4$repid) %>% sapply(function(xx) {
+  xx$c1sd <- sd(subset(xx,col==1)$value); 
+  xx$c1ctr <- rep(subset(xx,col==1)$value,each=12);
+  xx$c1med <- median(subset(xx,col==1)$value);
+  xx},simplify=F) %>% do.call('rbind',.) -> dat5;
+
+#' Get a matrix of plate-wise standard deviations
+qc.scale<-sapply(dat,function(xx) 
+  sapply(xx,function(yy) 
+    sd(apply(yy[,1:11],1,function(zz) zz[-1]-zz[1]))));
+
+#' Ditto for means
+qc<-sapply(dat,function(xx) 
+  sapply(xx,function(yy) 
+    mean(apply(yy[,1:11],1,function(zz) zz[-1]-zz[1]))));
+
+#' Mixed effects as alternative to manual normalization. The idea is you fit an
+#' `lme` model and then treat the _residuals_ as your response variable without 
+#' further normalization
+fit <- lme(value~treat,dat6<-subset(dat5,!Symbol%in%c('Mock','siPLK','Control')),
+           random=~1|ID/repid/col);
+dat7 <- groupedData(value~treat|ID,dat6);
+fit0 <- lme(value~treat,dat7,random=pdBlocked(list(pdIdent(~1),pdIdent(~row-1),pdIdent(~col-1))));
+summary(fit);
+plot(fit,level=3);
+dat6$norm<-residuals(fit,level=3);
+#ggplot(dat6,aes(x=Symbol,y=norm,col=treat))+geom_boxplot();
+
+#' Long story, will explain later, but this is kind of interesting
+#ggplot(transform(subset(dat5,ID!='1'&Symbol!='siPLK'),row=as.numeric(factor(row)),col=as.numeric(col),ID=as.numeric(ID),value=(value-c1med)/c1sd),aes(x=nwell,y=value,color=treat,group=repid))+geom_line()+facet_wrap(~ID);
